a719d991d1d8e4114837a3ee16f447c32cddade2
Noahma15 <noah.mahler@freenet.de>
2025-04-03 19:29:01 +0200

    Add files via upload

--- Graph.py
+++ Graph.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 reader = csv.reader(open("stats_all.csv", "r"), delimiter=";")
 stats_all = list(reader)
 
-specific_stat = 3
-specific_player = 17
+specific_stat = 0
+specific_player = 11
+specific_player_comparison = 8
+compare = True
 
 ######################  Correspondens table    ############################################################################
 #                                                                                                                         #
@@ -26,11 +28,17 @@ specific_player = 17
 
 #misc variables 
 average_stat = 0
+average_stat_comparison = 0
 times_played = 0
+times_played_comparison = 0
 stat_cumulative = 0
+stat_cumulative_comparison = 0
 specific_player_iterate = specific_player
+specific_player_comparison_iterate = specific_player_comparison
 game_array = []
+game_array_comparison = []
 game_stats_array = []
+game_stats_array_comparison = []
 list_playerNames = ['Jens','Luiz','Alex','Elias','Chris','Sebi','Simon','Steffen','Lukas','Marcel','Marian','Noah','Flo',
                     'Basti','Martin','Joel','Dirk','Herrn 2','Gegner']
 list_opponents = ['Botnang - Hinspiel','Untersteinbach - Hinspiel','Kornwestheim - Hinspiel','Stromberg - Hinspiel','AlliH3 - Rückspiel',
@@ -41,23 +49,61 @@ list_of_stats = ['Attack Kills','Attack Errors','Attack Attempts','Attack Atk%',
                 'Receive Attempts','Receive ?','Receive Pass%','Set Ast','Set Attempts','Set Errors','Dig Succesful','Dig Error',
                 'Block Kill','Block Attempts ','Block Error ','Block B/S ','Sets Played']
 
-for i in range(11):
-    stat = float(stats_all[specific_player_iterate][specific_stat])
-    if stat == -100: #checks if person played
-        stat_cumulative  +=0
-        game_array.append(0)
-        game_stats_array.append(0)
-    else:
-        stat_cumulative  +=stat
-        times_played +=1
-        game_array.append(1)
-        game_stats_array.append(stat)
+if compare == True:  
+    for i in range(11):
+        stat = float(stats_all[specific_player_iterate][specific_stat])
+        stat_comparison = float(stats_all[specific_player_comparison_iterate][specific_stat])
+        print(stat_comparison)
 
-    specific_player_iterate += 19
+        if stat == -100: #checks if person played
+            stat_cumulative  +=0
+            game_array.append(0)
+            game_stats_array.append(0)
+        else:
+            stat_cumulative  +=stat
+            times_played +=1
+            game_array.append(1)
+            game_stats_array.append(stat)
+        
+        if stat_comparison == -100: #checks if comparison played
+            stat_cumulative_comparison  +=0
+            game_array_comparison.append(0)
+            game_stats_array_comparison.append(0)
+        else:
+            stat_cumulative_comparison  +=stat_comparison
+            times_played_comparison +=1
+            game_array_comparison.append(1)
+            game_stats_array_comparison.append(stat_comparison)
 
-average_stat = stat_cumulative/times_played
+        specific_player_iterate += 19
+        specific_player_comparison_iterate += 19
 
-plt.plot(range(11),game_stats_array)
-plt.title('Statiskik: '+ list_of_stats[specific_stat] +'; Spieler: '+ list_playerNames[specific_player]+ '; Durchscnitt: '+str(round(average_stat,3))) 
+    average_stat = stat_cumulative/times_played
+    average_stat_comparison = stat_cumulative_comparison/times_played_comparison
+    
+else:
+    for i in range(11):
+        stat = float(stats_all[specific_player_iterate][specific_stat])
+        stat_comparison = float(stats_all[specific_player_comparison_iterate][specific_stat])
+        if stat == -100: #checks if person played
+            stat_cumulative  +=0
+            game_array.append(0)
+            game_stats_array.append(0)
+        else:
+            stat_cumulative  +=stat
+            times_played +=1
+            game_array.append(1)
+            game_stats_array.append(stat)
+
+        specific_player_iterate += 19
+    average_stat = stat_cumulative/times_played
+
+plt.plot(range(11),game_stats_array, label = 'Spieler: '+ list_playerNames[specific_player]+
+         '\n '+'Durchscnitt: '+str(round(average_stat,3)))
+if compare == True:
+    plt.plot(range(11),game_stats_array_comparison, label = 'Vergleich: '+ list_playerNames[specific_player_comparison]+
+             '\n '+'Durchscnitt: '+str(round(average_stat_comparison,3)))
+plt.title('Statiskik: '+ list_of_stats[specific_stat]) 
+plt.legend()
 plt.grid(True)
 plt.show()
